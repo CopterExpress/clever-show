a1a901a75f1236435473f4f7fb2dcfd3a46f523a
Arthur <goldartt@gmail.com>
2019-03-11 12:27:46 +0300

    Create QT gui files for server with new gui

---
+++ Server/server_gui.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'server_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.12
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(850, 460)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(680, 20, 150, 40))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(680, 120, 150, 40))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.spinBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox.setGeometry(QtCore.QRect(760, 70, 50, 40))
+        self.spinBox.setObjectName("spinBox")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(680, 170, 150, 40))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(680, 220, 150, 40))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(680, 280, 150, 40))
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(680, 330, 150, 40))
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(680, 380, 150, 40))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(680, 70, 71, 40))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(820, 70, 10, 40))
+        self.label_2.setObjectName("label_2")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(20, 20, 640, 400))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 850, 25))
+        self.menubar.setObjectName("menubar")
+        self.menuOptions = QtWidgets.QMenu(self.menubar)
+        self.menuOptions.setObjectName("menuOptions")
+        MainWindow.setMenuBar(self.menubar)
+        self.actionSend_Animations = QtWidgets.QAction(MainWindow)
+        self.actionSend_Animations.setObjectName("actionSend_Animations")
+        self.actionSend_Configurations = QtWidgets.QAction(MainWindow)
+        self.actionSend_Configurations.setObjectName("actionSend_Configurations")
+        self.menuOptions.addAction(self.actionSend_Animations)
+        self.menuOptions.addAction(self.actionSend_Configurations)
+        self.menubar.addAction(self.menuOptions.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Clever Drone Animation Player"))
+        self.pushButton.setText(_translate("MainWindow", "Preflight check"))
+        self.pushButton_2.setText(_translate("MainWindow", "Start animation"))
+        self.pushButton_3.setText(_translate("MainWindow", "Pause"))
+        self.pushButton_4.setText(_translate("MainWindow", "Stop"))
+        self.pushButton_5.setText(_translate("MainWindow", "Takeoff"))
+        self.pushButton_6.setText(_translate("MainWindow", "Land"))
+        self.pushButton_7.setText(_translate("MainWindow", "Disarm"))
+        self.label.setText(_translate("MainWindow", "Start after"))
+        self.label_2.setText(_translate("MainWindow", "s"))
+        self.menuOptions.setTitle(_translate("MainWindow", "Actions"))
+        self.actionSend_Animations.setText(_translate("MainWindow", "Send Animations"))
+        self.actionSend_Configurations.setText(_translate("MainWindow", "Send Configurations"))
+
+
