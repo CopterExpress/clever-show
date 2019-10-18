b3cd398012a4a275cf12691fed643adfadaa7568
Arthur Golubtsov <goldartt@gmail.com>
2019-10-18 14:56:11 +0300

    Server: Add SO_KEEPALIVE option to server socket

--- Server/server.py
+++ Server/server.py
@@ -39,6 +39,7 @@ class Server(messaging.Singleton):
         self.sel = selectors.DefaultSelector()
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
         self.server_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
         self.host = socket.gethostname()
