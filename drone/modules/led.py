eab82d2e4ae4e354b153937af79e7f58a10f5f25
Arthur Golubtsov <goldartt@gmail.com>
2020-06-20 04:02:06 +0300

    drone: Add service availability check to led module

--- drone/modules/led.py
+++ drone/modules/led.py
@@ -1,4 +1,13 @@
 import rospy
+import logging
 from clover.srv import SetLEDEffect
 
-set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)
+logger = logging.getLogger(__name__)
+
+set_effect_service = rospy.ServiceProxy('led/set_effect', SetLEDEffect)
+
+def set_effect(*args, **kwargs):
+    try:
+        set_effect_service(*args, **kwargs)
+    except rospy.ServiceException:
+        logger.error("Can't set led effect: service /led/set_effect is unavailable!")
