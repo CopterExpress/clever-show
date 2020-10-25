42bb55d78f30f6e32bcc2fd55a2f877534166fe5
Artem30801 <artem30801@gmail.com>
2020-10-25 21:12:46 +0300

    tests: added basic messaging test

---
+++ tests/messaging_test.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pytest
+current_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.realpath(os.path.join(current_dir, os.pardir)))
+
+from lib.messaging import CallbackManager
+
+def test_callback_registration():
+    def f(arg):
+        return arg
+    c = CallbackManager()
+    c.action_callback("123")(f)
+    t = "test"
+    assert t == c.action_callbacks["123"](t)
